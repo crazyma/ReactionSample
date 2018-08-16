98b432d4e8c543d0cee9f8f159390d6f483dcd54
Badu <badu@dcard.cc>
2018-08-16 18:17:24 +0800

    create TrickerView

---
+++ app/src/main/java/com/dcard/reactionsample/TrickerView.kt
@@ -0,0 +1,18 @@
+package com.dcard.reactionsample
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class TrickerView @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return super.onTouchEvent(event)
+    }
+
+}
