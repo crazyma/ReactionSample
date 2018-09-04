bb8426d0116e40924372c48e798dc55b9fbb2171
Badu <badu@dcard.cc>
2018-09-04 16:56:15 +0800

    modify reaction size when dragging

--- app/src/main/java/com/dcard/reactionsample/AvatarsView.kt
+++ app/src/main/java/com/dcard/reactionsample/AvatarsView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 
@@ -15,58 +16,58 @@ class AvatarsView @JvmOverloads constructor(
 
     var flag = 0
 
-    var interval = 0
+    var originalSize = 0
+    var biggerSize = 0
+    var smallerSize = 0
     val reaction1 = ContextCompat.getDrawable(context, R.drawable.reaction_smile)
     val reaction2 = ContextCompat.getDrawable(context, R.drawable.reaction_chu)
     val reaction3 = ContextCompat.getDrawable(context, R.drawable.reaction_crazy)
     val reaction4 = ContextCompat.getDrawable(context, R.drawable.reaction_noface)
     val reaction5 = ContextCompat.getDrawable(context, R.drawable.reaction_cry)
 
-    init{
+    val reactionCount = 5
+    val xBoundList = mutableListOf<Int>()
+    val yBoundList = mutableListOf<Int>()
 
+    init {
+        xBoundList.add(originalSize)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
-        interval = w / 5
+        val count = 5
+        originalSize = w / count
+        biggerSize = originalSize * 2
+        smallerSize = (w - biggerSize) / (count - 1)
+
+        modifyBounding(-1)
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        when(flag){
-            0 -> {
-                reaction1?.run {
-                    setBounds(0,0,interval,height)
-                    draw(canvas)
-                }
-            }
-            1 -> {
-                reaction2?.run {
-                    setBounds(interval,0,2 * interval,height)
-                    draw(canvas)
-                }
-            }
-            2 -> {
-                reaction3?.run {
-                    setBounds(2 * interval,0,3 * interval,height)
-                    draw(canvas)
-                }
-            }
-            3 -> {
-                reaction4?.run {
-                    setBounds(3 * interval,0,4 * interval,height)
-                    draw(canvas)
-                }
-            }
-            4 -> {
-                reaction5?.run {
-                    setBounds(4 * interval,0,width,height)
-                    draw(canvas)
-                }
-            }
+        reaction1?.run {
+            setBounds(0, yBoundList[0], xBoundList[0], height)
+            draw(canvas)
         }
+        reaction2?.run {
+            setBounds(xBoundList[0], yBoundList[1], xBoundList[1], height)
+            draw(canvas)
+        }
+        reaction3?.run {
+            setBounds(xBoundList[1], yBoundList[2], xBoundList[2], height)
+            draw(canvas)
+        }
+        reaction4?.run {
+            setBounds(xBoundList[2], yBoundList[3], xBoundList[3], height)
+            draw(canvas)
+        }
+        reaction5?.run {
+            setBounds(xBoundList[3], yBoundList[4], width, height)
+            draw(canvas)
+        }
+
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -74,47 +75,111 @@ class AvatarsView @JvmOverloads constructor(
         val x = event.x.toInt()
         val y = event.y.toInt()
 
-        when(event.action){
+        when (event.action) {
             MotionEvent.ACTION_MOVE -> {
-                if(y in 0..height) {
+                if (y in 0..height) {
                     when {
-                        x <= interval -> {
-                            if(flag != 0){
+                        x <= originalSize -> {
+                            if (flag != 0) {
                                 flag = 0
+                                modifyBounding(flag)
                                 invalidate()
                             }
-
                         }
-                        x <= interval * 2 -> {
-                            if(flag != 1){
+                        x <= originalSize * 2 -> {
+                            if (flag != 1) {
                                 flag = 1
+                                modifyBounding(flag)
                                 invalidate()
                             }
                         }
-                        x <= interval * 3 -> {
-                            if(flag != 2){
+                        x <= originalSize * 3 -> {
+                            if (flag != 2) {
                                 flag = 2
+                                modifyBounding(flag)
                                 invalidate()
                             }
                         }
-                        x <= interval * 4 -> {
-                            if(flag != 3){
+                        x <= originalSize * 4 -> {
+                            if (flag != 3) {
                                 flag = 3
+                                modifyBounding(flag)
                                 invalidate()
                             }
                         }
                         x <= width -> {
-                            if(flag != 4){
+                            if (flag != 4) {
                                 flag = 4
+                                modifyBounding(flag)
                                 invalidate()
                             }
                         }
+                        else -> {
+                            if (flag != -1) {
+                                flag = -1
+                                modifyBounding(-1)
+                                invalidate()
+                            }
+                        }
+                    }
+                } else {
+                    if (flag != -1) {
+                        flag = -1
+                        modifyBounding(-1)
+                        invalidate()
                     }
                 }
             }
         }
 
         return true
+    }
+
+    private fun modifyBounding(flag: Int){
+        modifyYBounding(flag)
+        modifyXBounding(flag)
+    }
+
+    private fun modifyXBounding(flag: Int) {
+        xBoundList.clear()
+        var total = 0
+        if (flag == -1) {
+            for (i in 0 until reactionCount - 1) {
+                total += originalSize
+                xBoundList.add(total)
+            }
+        } else {
+
+            for (i in 0 until reactionCount - 1) {
+                total += if (flag == i) {
+                    biggerSize
+                } else {
+                    smallerSize
+                }
+
+                xBoundList.add(total)
+            }
+        }
+    }
+
+    private fun modifyYBounding(flag: Int) {
+        yBoundList.clear()
+        val offsetForOriginalSize = height - originalSize
+        val offsetForBiggerSize = height - biggerSize
+        val offsetForSmallSize = height - smallerSize
+
+
+        for (i in 0..reactionCount) {
+            if (flag == -1) {
+                yBoundList.add(offsetForOriginalSize)
+            } else {
+                if (i == flag) {
+                    yBoundList.add(offsetForBiggerSize)
+                } else {
+                    yBoundList.add(offsetForSmallSize)
+                }
+            }
+        }
     }
 
 
