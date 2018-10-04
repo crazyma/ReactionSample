eb38caf6fd1604260a8e37c7420f5ca7d60f0ca7
Badu <badu@dcard.cc>
2018-10-04 14:44:05 +0800

    create spark effect with 12 dots

--- app/src/main/java/com/dcard/reactionsample/spark/SparkActivity.kt
+++ app/src/main/java/com/dcard/reactionsample/spark/SparkActivity.kt
@@ -5,13 +5,13 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.dcard.reactionsample.R
-import kotlinx.android.synthetic.main.layout_spark.*
+import kotlinx.android.synthetic.main.activity_spark.*
 
 class SparkActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_spark)
+        setContentView(R.layout.activity_spark)
 
         val bitmap2 = BitmapFactory.decodeResource(resources, R.drawable.reaction_chu)
         sparkView.iconBitmap = bitmap2
--- app/src/main/java/com/dcard/reactionsample/spark/SparkView.kt
+++ app/src/main/java/com/dcard/reactionsample/spark/SparkView.kt
@@ -20,34 +20,43 @@ class SparkView @JvmOverloads constructor(
     private val dp = Resources.getSystem().displayMetrics.density
 
     val iconPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    val biggerCirclePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    val circlePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.BLACK
+        style = Paint.Style.STROKE
+    }
+    val dotPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = Color.BLACK
         style = Paint.Style.FILL
     }
-    val smallCirclePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        color = Color.WHITE
-        style = Paint.Style.FILL
-    }
+
+    val iconSize: Int = (24 * dp).toInt()
 
 
-    var iconSize: Int = (24 * dp).toInt()
+    val dotStartDistance = 16f * dp
+    val dotEndDistance = 24f * dp
+    val dotStartRadius = 4f * dp
+    val dotEndRadius = 0f
+    val circleStartRadius = iconSize / 4f
+    val circleEndRadius = 18f * dp
+    val circleStartStrokeWidth = iconSize / 4f
+    val circleEndStrokeWidth = 0f
+
+    //  Current Variable
     var iconCurrentSize = iconSize
-
-
-    var biggerCircleCurrentRadius = 0f
-    var smallerCircleCurrentRadius = 0f
-    var biggerCircleStartRadius = iconSize / 2f
-    var smallerCircleStartRadius = 0f
-    val maxCircleRadius = 24f * dp
+    var currentCircleRadius = 0f
+    var currentCircleStrokeWidth = circleStartStrokeWidth
+    var dotCurrentDistance = 0f
+    var biggerDotCurrentRadius = 0f
+    var smallerDotCurrentRadius = 0f
 
     var centerX = 0
     var centerY = 0
 
     var iconBitmap: Bitmap? = null
-     set(value) {
-         field = value
-         postInvalidate()
-     }
+        set(value) {
+            field = value
+            postInvalidate()
+        }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -55,14 +64,17 @@ class SparkView @JvmOverloads constructor(
         centerY = h / 2
     }
 
-    fun runSpark(){
+    fun runSpark() {
         ValueAnimator.ofFloat(0f, 1f).apply {
-            duration = 200
+            duration = 500
             interpolator = LinearInterpolator()
             addUpdateListener {
                 val fraction = it.animatedValue as Float
 
                 calculateCircleRadius(fraction)
+                calculateDotDistance(fraction)
+                calculateBiggerDotAppearance(fraction)
+                calculateSmallerAppearance(fraction)
 
                 postInvalidate()
             }
@@ -71,6 +83,8 @@ class SparkView @JvmOverloads constructor(
                 override fun onAnimationRepeat(animation: Animator?) {}
 
                 override fun onAnimationEnd(animation: Animator?) {
+                    currentCircleRadius = 0f
+                    postInvalidate()
                 }
 
                 override fun onAnimationCancel(animation: Animator?) {}
@@ -85,8 +99,26 @@ class SparkView @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        canvas.drawCircle(centerX.toFloat(), centerY.toFloat(), biggerCircleCurrentRadius, biggerCirclePaint)
-        canvas.drawCircle(centerX.toFloat(), centerY.toFloat(), smallerCircleCurrentRadius, smallCirclePaint)
+        // smaller dot
+        for (i in 0 until 6) {
+            val angdeg = 60.0 * i
+            val xx = dotCurrentDistance * Math.cos(Math.toRadians(angdeg))
+            val yy = dotCurrentDistance * Math.sin(Math.toRadians(angdeg))
+            canvas.drawCircle(centerX.toFloat() + xx.toFloat(), centerY.toFloat() - yy.toFloat(), smallerDotCurrentRadius, dotPaint)
+        }
+
+        //  bigger dot
+        for (i in 0 until 6) {
+            val angdeg = 60.0 * i + 30.0
+            val xx = dotCurrentDistance * Math.cos(Math.toRadians(angdeg))
+            val yy = dotCurrentDistance * Math.sin(Math.toRadians(angdeg))
+            canvas.drawCircle(centerX.toFloat() + xx.toFloat(), centerY.toFloat() - yy.toFloat(), biggerDotCurrentRadius, dotPaint)
+        }
+
+        circlePaint.strokeWidth = currentCircleStrokeWidth
+        if(circlePaint.strokeWidth > 0f) {
+            canvas.drawCircle(centerX.toFloat(), centerY.toFloat(), currentCircleRadius, circlePaint)
+        }
 
         iconBitmap?.run {
             val offset = iconCurrentSize / 2
@@ -94,11 +126,30 @@ class SparkView @JvmOverloads constructor(
         }
     }
 
-    private fun calculateCircleRadius(fraction: Float){
+    private fun calculateCircleRadius(fraction: Float) {
+        val v = when{
+            fraction <= .5f -> {fraction * 2f}
+            else -> {1f}
+        }
 
-        biggerCircleCurrentRadius = calculateInterpolatedValue(biggerCircleStartRadius, maxCircleRadius, fraction)
-        smallerCircleCurrentRadius = calculateInterpolatedValue(smallerCircleStartRadius, maxCircleRadius, fraction)
+        currentCircleRadius = calculateInterpolatedValue(circleStartRadius, circleEndRadius, v)
+        currentCircleStrokeWidth = calculateInterpolatedValue(circleStartStrokeWidth, circleEndStrokeWidth, v)
+    }
 
+    private fun calculateDotDistance(fraction: Float) {
+        dotCurrentDistance = calculateInterpolatedValue(dotStartDistance, dotEndDistance, fraction)
+    }
+
+    private fun calculateBiggerDotAppearance(fraction: Float) {
+        biggerDotCurrentRadius = calculateInterpolatedValue(dotStartRadius, dotEndRadius, fraction)
+    }
+
+    private fun calculateSmallerAppearance(fraction: Float) {
+        val v = when{
+            fraction <= .5f -> {fraction * 2f}
+            else -> {1f}
+         }
+        smallerDotCurrentRadius = calculateInterpolatedValue(dotStartRadius, dotEndRadius, v)
     }
 
     private fun calculateInterpolatedValue(start: Float, end: Float, fraction: Float) =
