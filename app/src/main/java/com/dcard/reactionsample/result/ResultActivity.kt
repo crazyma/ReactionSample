b13a2df85abc801ca7d3b834a0605bf5db7a6455
David-Retina15 <david77115@gmail.com>
2018-10-06 12:39:31 +0800

    create reaction result reyclerview and adpater

--- app/src/main/java/com/dcard/reactionsample/result/ResultActivity.kt
+++ app/src/main/java/com/dcard/reactionsample/result/ResultActivity.kt
@@ -2,12 +2,22 @@ package com.dcard.reactionsample.result
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
 import com.dcard.reactionsample.R
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity:AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        setupRecyclerView()
     }
+
+    private fun setupRecyclerView() {
+        recyclerView.layoutManager = GridLayoutManager(this,2)
+        recyclerView.adapter = ReactionResultAdapter(this)
+    }
+
+
 }
