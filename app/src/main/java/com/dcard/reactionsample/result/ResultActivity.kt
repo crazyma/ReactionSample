2dde8e4112d0562292995d74a3256951a2e5c43a
David-Retina15 <david77115@gmail.com>
2018-10-06 17:53:24 +0800

    create ReactionResultBaseLayout

--- app/src/main/java/com/dcard/reactionsample/result/ResultActivity.kt
+++ app/src/main/java/com/dcard/reactionsample/result/ResultActivity.kt
@@ -11,7 +11,7 @@ class ResultActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-        setupRecyclerView()
+//        setupRecyclerView()
     }
 
     private fun setupRecyclerView() {
