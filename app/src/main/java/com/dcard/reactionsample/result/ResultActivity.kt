2653d7ee2b996ceb6cc3958ac2a09d204fa5783a
David-Retina15 <david77115@gmail.com>
2018-10-06 12:14:14 +0800

    create ResultActivity

---
+++ app/src/main/java/com/dcard/reactionsample/result/ResultActivity.kt
@@ -0,0 +1,13 @@
+package com.dcard.reactionsample.result
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.dcard.reactionsample.R
+
+class ResultActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
