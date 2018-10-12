f6f53ba89bf5a83aa8df0eafbf76998fed108c3f
Badu <badu@dcard.cc>
2018-10-12 15:12:48 +0800

    modify detail of ReactionBarView

--- app/src/main/java/com/dcard/reactionsample/reactionbar/ReactionBarActivity.kt
+++ app/src/main/java/com/dcard/reactionsample/reactionbar/ReactionBarActivity.kt
@@ -1,14 +1,27 @@
 package com.dcard.reactionsample.reactionbar
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import com.dcard.reactionsample.R
+import kotlinx.android.synthetic.main.activity_reaction_bar.*
 
-class ReactionBarActivity: AppCompatActivity() {
+class ReactionBarActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reaction_bar)
+
+        Handler().post {
+            val bitmap1 = BitmapFactory.decodeResource(resources, R.drawable.reaction_smile)
+            val bitmap2 = BitmapFactory.decodeResource(resources, R.drawable.reaction_chu)
+            val bitmap5 = BitmapFactory.decodeResource(resources, R.drawable.reaction_cry)
+            reactionBarView.addBitmap(listOf(bitmap1, bitmap2, bitmap5))
+        }
+
     }
 
 }
--- app/src/main/java/com/dcard/reactionsample/reactionbar/ReactionBarView.kt
+++ app/src/main/java/com/dcard/reactionsample/reactionbar/ReactionBarView.kt
@@ -1,11 +1,9 @@
 package com.dcard.reactionsample.reactionbar
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Resources
 import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import com.dcard.reactionsample.R
 
@@ -21,10 +19,10 @@ class ReactionBarView @JvmOverloads constructor(
         private const val OFFSET = 4
     }
 
-
     val bitmap1 = BitmapFactory.decodeResource(resources, R.drawable.reaction_smile)
     val bitmap2 = BitmapFactory.decodeResource(resources, R.drawable.reaction_chu)
     val bitmap5 = BitmapFactory.decodeResource(resources, R.drawable.reaction_cry)
+
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val dp = Resources.getSystem().displayMetrics.density
     private val iconSize = ICON_SIZE * dp
@@ -33,54 +31,63 @@ class ReactionBarView @JvmOverloads constructor(
     private val maskRadius = maskSize / 2
     private val offset = OFFSET * dp
     private val list = mutableListOf<Bitmap>()
+    private var rectF: RectF
+    private val xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OUT)
 
     init {
-        Log.d("badu", "icon size : $iconSize")
-        list.add(bitmap1)
-        list.add(bitmap2)
-        list.add(bitmap5)
+        rectF = RectF(0f, 0f, getWidth(list.size), iconSize)
+    }
+
+    fun addBitmap(bitmap: Bitmap) {
+        list.add(bitmap)
+        calculateRectF()
+        requestLayout()
+        invalidate()
+    }
+
+    fun addBitmap(l: List<Bitmap>) {
+        list.clear()
+        list.addAll(l)
+        calculateRectF()
+        requestLayout()
+        postInvalidate()
+    }
+
+    fun clearBitmap() {
+        list.clear()
+        calculateRectF()
+        requestLayout()
+        invalidate()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-
         setMeasuredDimension(getWidth(list.size).toInt(), iconSize.toInt())
     }
 
-    @SuppressLint("DrawAllocation")
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-//        setLayerType(View.LAYER_TYPE_SOFTWARE, null)
-        val layerID = canvas.saveLayer(RectF(0f, 0f, getWidth(list.size), iconSize), paint)
+        //  setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+        val layerID = canvas.saveLayer(rectF, paint)
 
         var circleX: Float
-        for(i in list.size - 1 downTo 0){
+        for (i in list.size - 1 downTo 0) {
             circleX = getCircleX(i)
-            Log.d("badu","circleX : $circleX")
-            canvas.drawBitmap(list[i], null, RectF(circleX - radius, 0f, circleX + radius, iconSize), paint)
-            paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OUT)
-            if(i != 0){
-                circleX = getCircleX(i -1)
+
+            canvas.drawBitmap(list[i], null, getBitmapRecF(circleX), paint)
+            paint.xfermode = xfermode
+            if (i != 0) {
+                circleX = getCircleX(i - 1)
                 canvas.drawCircle(circleX, radius, maskRadius, paint)
                 paint.xfermode = null
             }
         }
         canvas.restoreToCount(layerID)
-
-//        Log.d("badu","circleX :")
-//        canvas.drawBitmap(list[1], null, RectF(getCircleX(1) - radius, 0f, getCircleX(1) + radius, iconSize), paint)
-//        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OUT)
-//        canvas.drawCircle(getCircleX(0), radius, maskRadius, paint)
-//        paint.xfermode = null
-//        canvas.drawBitmap(list[0], null, RectF(getCircleX(0) - radius, 0f, getCircleX(0) + radius, iconSize), paint)
-//
-//        canvas.restoreToCount(layerID)
     }
 
-    fun getMaskedBitmap() {
-        val canvas = Canvas(bitmap1)
-        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_OUT)
-        canvas.drawBitmap(bitmap2, null, Rect(60, 0, 72 + 60, 72), paint)
+    private fun calculateRectF() {
+        rectF = RectF(0f, 0f, getWidth(list.size), iconSize)
     }
 
     private fun getCircleX(n: Int) =
@@ -91,5 +98,8 @@ class ReactionBarView @JvmOverloads constructor(
                 0 -> 0f
                 else -> getCircleX(size - 1) + radius
             }
+
+    private fun getBitmapRecF(circleX: Float) =
+            RectF(circleX - radius, 0f, circleX + radius, iconSize)
 
 }
